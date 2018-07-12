2e1f61c095a0da1fe67920dd47871dec106138b7
Joshua Nicholas Pearson <jnpearson4850@gmail.com>
2018-07-12 09:53:25 -0400

    starting to add javascript

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
+var level = document.getElementsByClassName(level)
+var rings = document.getElementsByClassName(rings)
+var circle1 = document.getElementsByClassName(circle1)
+var circle2 = document.getElementsByClassName(circle2)
+var circle3 = document.getElementsByClassName(circle3)
+var circle4 = document.getElementsByClassName(circle4)
+var circle5 = document.getElementsByClassName(circle5)
+var start = document.getElementsByClassName(start)
+var reset = document.getElementsByClassName(reset)
 
-// const startButton = document.querySelector('.start')
-// startButton.addEventListener("click", handleClickEvent)
+var sequence = ["green", "red", "blue", "orange", "purple"]
+
+function random(sequence) {
+    for (var i = 0; i < sequence.length; i++)
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// start.addEventListener("click", handleClickEvent)
 
 // function handleClickEvent () {
-//     document.body.style.background = "red";
+// console.log("I was clicked")
 // }
+// const resetButton = document.getElementsByClassName('.reset')
+// resetButton.addEventListener("click", handleClickEvent)
+
+// function handleClickEvent () {
+//     document.body.style.background = "bule";
+// }
+
